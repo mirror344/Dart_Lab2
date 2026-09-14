5d28b4ea0657f0a02f83ab475f0c7d4fd50e2ab4
Магдеев <dudakovivan3@gmail.com>
2026-09-14 11:36:06 +0300

    Started ToDo CLI app

--- bin/lab2_todo.dart
+++ bin/lab2_todo.dart
@@ -1,17 +1,50 @@
+import 'dart:io';
 import 'package:lab2_todo/todo.dart';
+
+void printMenu() {
+  print(' ');
+  print('ToDo список');
+  print('add - добавить задачу');
+  print('list - показать все задачи');
+  print('done - отменить выполнение');
+  print('delete - удалить задачу');
+  print('exit - выйти');
+}
+
+void addTodo(List<Todo> todos) {
+  stdout.write('Название задачи:');
+  String? input = stdin.readLineSync();
+
+  if (input == null || input.trim().isEmpty) {
+    print('Ошибка: название не может быть пустым');
+    return;
+  }
+  int newId = todos.isEmpty ? 1 : todos.last.id +1;
+  todos.add(Todo(id: newId, title: input.trim()));
+  print('Задача добавлена!');
+}
+
+void listTodos(List<Todo> todos) {
+  if (todos.isEmpty) {
+    print('Список задач пуст');
+    return;
+  }
+  print(' ');
+  for (var todo in todos) {
+    print(todo);
+  }
+}
+
+void completeTodo(List<Todo> todos) {
+  
+}
+
 
 
 void main(List<String> arguments) {
-  Todo task1 = Todo(
-    id: 1, 
-    title: 'Купить продукты'
-  );
-  Todo task2 = Todo(
-    id: 2, 
-    title: 'Сделать зарядку'
-  );
-  task1.complete();
-  print(task1);
-  print(task2);
+  stdout.write('Введите что-нибудь');
+  String? input = stdin.readLineSync();
+  print('Вы ввели: $input');
+
 }
 
