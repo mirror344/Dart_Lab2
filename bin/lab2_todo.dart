3430c1c3e37f05e8907726b6dcb011bc4b472f56
mirror344 <dudakovivan3@gmail.com>
2026-09-21 00:16:01 +0300

    Add getter and static counter to Todo

--- bin/lab2_todo.dart
+++ bin/lab2_todo.dart
@@ -19,8 +19,8 @@ void addTodo(List<Todo> todos) {
     print('Ошибка: название не может быть пустым');
     return;
   }
-  int newId = todos.isEmpty ? 1 : todos.last.id +1;
-  todos.add(Todo(id: newId, title: input.trim()));
+  // int newId = todos.isEmpty ? 1 : todos.last.id +1;
+  todos.add(Todo(title: input.trim()));
   print('Задача добавлена!');
 }
 
--- lib/todo.dart
+++ lib/todo.dart
@@ -2,13 +2,16 @@ class Todo{
   int id;
   String title;
   bool isDone;
+  
+  String get status => isDone ? 'выполнено' : 'в процессе';
 
+  static int _counter = 0;
+  Todo({required this.title}) : id = ++_counter, isDone = false;
 
-  Todo({required this.id, required this.title}) : isDone = false;
   @override
   String toString() {
-  String status = isDone ? '[x]' : '[ ]';
-  return '$status $id. $title';
+  String mark = isDone ? '[x]' : '[ ]';
+  return '$mark $id. $title ($status)';
   }
 
   void complete() {
