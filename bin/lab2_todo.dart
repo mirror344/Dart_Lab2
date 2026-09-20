0fe7f2ecda9ce370fc0f4e691beb672e245c35fc
mirror344 <dudakovivan3@gmail.com>
2026-09-21 00:28:00 +0300

    add package ansicolor

--- bin/lab2_todo.dart
+++ bin/lab2_todo.dart
@@ -1,84 +1,91 @@
 import 'dart:io';
 import 'package:lab2_todo/todo.dart';
+import 'package:ansicolor/ansicolor.dart';
+
+final AnsiPen greenPen = AnsiPen()..green();
+final AnsiPen redPen = AnsiPen()..red();
+final AnsiPen bluePen = AnsiPen()..blue();
+final AnsiPen yellowPen = AnsiPen()..yellow();
+
 
 void printMenu() {
   print(' ');
-  print('ToDo список');
-  print('add - добавить задачу');
-  print('list - показать все задачи');
-  print('done - отменить выполнение');
-  print('delete - удалить задачу');
-  print('exit - выйти');
+  print(yellowPen(('ToDo список')));
+  print(yellowPen(('add - добавить задачу')));
+  print(yellowPen(('list - показать все задачи')));
+  print(yellowPen(('done - отменить выполнение')));
+  print(yellowPen(('delete - удалить задачу')));
+  print(yellowPen(('exit - выйти')));
 }
 
 void addTodo(List<Todo> todos) {
-  stdout.write('Название задачи: ');
+  stdout.write(bluePen(('Название задачи: ')));
   String? input = stdin.readLineSync();
 
   if (input == null || input.trim().isEmpty) {
-    print('Ошибка: название не может быть пустым');
+    print(redPen(('Ошибка: название не может быть пустым')));
     return;
   }
   // int newId = todos.isEmpty ? 1 : todos.last.id +1;
   todos.add(Todo(title: input.trim()));
-  print('Задача добавлена!');
+  print(greenPen(('Задача добавлена!')));
 }
 
 void listTodos(List<Todo> todos) {
   if (todos.isEmpty) {
-    print('Список задач пуст');
+    print(redPen(('Список задач пуст')));
     return;
   }
   print(' ');
   for (var todo in todos) {
-    print(todo);
+    print(yellowPen((todo)));
   }
 }
 
 void completeTodo(List<Todo> todos) {
-  stdout.write('ID задачи: ');
+  stdout.write(bluePen(('ID задачи: ')));
   String? input = stdin.readLineSync();
 
   if (input == null) return;
   int? id = int.tryParse(input.trim());
 
   if (id == null) {
-    print('Ошибка: введите число');
+    print(redPen(('Ошибка: введите число')));
     return;
   }
 
   for (var todo in todos) {
     if (todo.id == id) {
       todo.complete();
-      print('Задача отмечена выполненной!');
+      print(greenPen(('Задача отмечена выполненной!')));
       return;
     }
   }
 
-  print('Задача с ID $id не найдена');
+  print(redPen(('Задача с ID $id не найдена')));
 }
 
 void deleteTodo(List<Todo> todos) {
-  stdout.write('ID задачи: ');
+  stdout.write(bluePen(('ID задачи: ')));
   String? input = stdin.readLineSync();
 
   if (input == null) return;
 
   int? id = int.tryParse(input.trim());
   if (id == null) {
-    print('Ошибка: введите число');
+    print(redPen(('Ошибка: введите число')));
     return;
   }
 
   for (int i = 0; i < todos.length; i++) {
     if (todos[i].id == id) {
       todos.removeAt(i);
-      print('Задача удалена!');
+      print(greenPen(('Задача удалена!')));
       return;
     }
   }
 
-  print('Задача с ID $id не найдена');
+  print(redPen(('Задача с ID $id не найдена')));
 }
 
 void main(List<String> arguments) {
@@ -98,8 +105,8 @@ void main(List<String> arguments) {
       case 'list': listTodos(todos); break;
       case 'done': completeTodo(todos); break;
       case 'delete': deleteTodo(todos); break;
-      case 'exit': print('До свидания!'); return;
-      default: printMenu(); print('Неизвестная команда');
+      case 'exit': print(yellowPen(('До свидания!'))); return;
+      default: printMenu(); print(redPen(('Неизвестная команда')));
     }
   }
 
